d3ff0c852de27bf545ed76fea0e9b3f0b9cde845
Andrey Shamis <lolnik@gmail.com>
2017-04-14 15:56:58 +0300

    Change added code
    Signed-off-by: Andrey Shamis <lolnik@gmail.com>

--- Boiler/Boiler.ino
+++ Boiler/Boiler.ino
@@ -559,101 +559,98 @@ void displaySensorDetails(void)
   sensor_t sensor;
   accel.getSensor(&sensor);
   Serial.println("------------------------------------");
-  Serial.print  ("Sensor:       "); Serial.println(sensor.name);
-  Serial.print  ("Driver Ver:   "); Serial.println(sensor.version);
-  Serial.print  ("Unique ID:    "); Serial.println(sensor.sensor_id);
-  Serial.print  ("Max Value:    "); Serial.print(sensor.max_value); Serial.println(" m/s^2");
-  Serial.print  ("Min Value:    "); Serial.print(sensor.min_value); Serial.println(" m/s^2");
-  Serial.print  ("Resolution:   "); Serial.print(sensor.resolution); Serial.println(" m/s^2");
-  Serial.println("------------------------------------");
-  Serial.println("");
-  delay(500);
+  Serial.println  ("Sensor:       " + String(sensor.name));
+  Serial.println  ("Driver Ver:   " + sensor.version);
+  Serial.println  ("Unique ID:    " + sensor.sensor_id);
+  Serial.println  ("Max Value:    " + String(sensor.max_value) + " m/s^2");
+  Serial.println  ("Min Value:    " + String(sensor.min_value) + " m/s^2");
+  Serial.println  ("Resolution:   " + String(sensor.resolution) + " m/s^2");
+  Serial.println("------------------------------------\n");
 }
 
 void displayDataRate(void)
 {
-  Serial.print  ("Data Rate:    ");
-
+  
+  String data_rate = "";
   switch (accel.getDataRate())
   {
     case ADXL345_DATARATE_3200_HZ:
-      Serial.print  ("3200 ");
+      data_rate = "3200";
       break;
     case ADXL345_DATARATE_1600_HZ:
-      Serial.print  ("1600 ");
+      data_rate = "1600";
       break;
     case ADXL345_DATARATE_800_HZ:
-      Serial.print  ("800 ");
+      data_rate = "800";
       break;
     case ADXL345_DATARATE_400_HZ:
-      Serial.print  ("400 ");
+      data_rate = "400";
       break;
     case ADXL345_DATARATE_200_HZ:
-      Serial.print  ("200 ");
+      data_rate = "200";
       break;
     case ADXL345_DATARATE_100_HZ:
-      Serial.print  ("100 ");
+      data_rate = "100";
       break;
     case ADXL345_DATARATE_50_HZ:
-      Serial.print  ("50 ");
+      data_rate = "50";
       break;
     case ADXL345_DATARATE_25_HZ:
-      Serial.print  ("25 ");
+      data_rate = "25";
       break;
     case ADXL345_DATARATE_12_5_HZ:
-      Serial.print  ("12.5 ");
+      data_rate = "12.5";
       break;
     case ADXL345_DATARATE_6_25HZ:
-      Serial.print  ("6.25 ");
+      data_rate = "6.25";
       break;
     case ADXL345_DATARATE_3_13_HZ:
-      Serial.print  ("3.13 ");
+      data_rate = "3.13";
       break;
     case ADXL345_DATARATE_1_56_HZ:
-      Serial.print  ("1.56 ");
+      data_rate = "1.56";
       break;
     case ADXL345_DATARATE_0_78_HZ:
-      Serial.print  ("0.78 ");
+      data_rate = "0.78";
       break;
     case ADXL345_DATARATE_0_39_HZ:
-      Serial.print  ("0.39 ");
+      data_rate = "0.39";
       break;
     case ADXL345_DATARATE_0_20_HZ:
-      Serial.print  ("0.20 ");
+      data_rate = "0.20";
       break;
     case ADXL345_DATARATE_0_10_HZ:
-      Serial.print  ("0.10 ");
+      data_rate = "0.10";
       break;
     default:
-      Serial.print  ("???? ");
+      data_rate = "???";
       break;
   }
-  Serial.println(" Hz");
+  Serial.println("Data Rate:  " + data_rate + " Hz");
 }
 
 void displayRange(void)
 {
-  Serial.print  ("Range:         +/- ");
-
+  String val = "";
   switch (accel.getRange())
   {
     case ADXL345_RANGE_16_G:
-      Serial.print  ("16 ");
+      val = "16";
       break;
     case ADXL345_RANGE_8_G:
-      Serial.print  ("8 ");
+      val = "8";
       break;
     case ADXL345_RANGE_4_G:
-      Serial.print  ("4 ");
+      val = "4";
       break;
     case ADXL345_RANGE_2_G:
-      Serial.print  ("2 ");
+      val = "2";
       break;
     default:
-      Serial.print  ("?? ");
+      val = "???";
       break;
   }
-  Serial.println(" g");
+  Serial.println("Range:  +/- " + val + " g");
 }
 
 
