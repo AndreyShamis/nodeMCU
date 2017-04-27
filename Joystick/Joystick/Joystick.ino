a25e165ec4157189691705c4608e840a2ad25e46
Andrey Shamis <andrey.shamis@intel.com>
2017-04-27 14:58:51 +0300

    Try code with 10K resistor
    https://hackaday.io/project/8435-2-analog-inputs-for-esp8266-without-multiplexer
    Firstly I hooked up the joystick as shown in the schematic.
    The two 10k resistors coming from Joystick X and Y are there so when the
    digital GPIO is sinked to GND the Analog input is not also sinked completely
    to ground. to check for X GPIO 13 is set to floating and GPIO 14 is set to GND.
    using those 2 resistors along the voltage at the ADC will be half
    of what is coming from the Joystick X which will be a maximum of 1.65V.
    Since the ADC will only read to a maximum of 1 Volt, this
    voltage needs to be brought down even more.
    An additional 10K resistor is used to bring the voltage down even further.
    Ideally this should be a smaller value but in testing 10K seemed to be adequate
    and this further brought down the BOM.
    Using this code I was able to make a function to calibrate the
    joystick by finding it's maximum and minimum values for both
    the X and Y and then creating functions for getting the X and the Y values.
    
    Signed-off-by: Andrey Shamis <andrey.shamis@intel.com>

--- Joystick/Joystick/Joystick.ino
+++ Joystick/Joystick/Joystick.ino
@@ -21,11 +21,12 @@
  http://www.arduino.cc/en/Tutorial/AnalogInput
 
  */
-
+#define MIN(A, B) ((A) < (B) ? (A) : (B))
+#define MAX(A, B) ((A) > (B) ? (A) : (B))
 //#define BLYNK_PRINT Serial    // Prints to serial
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
-#include <BlynkSimpleEsp8266.h>//Calls Blynk ESP8266 Library
+//#include <BlynkSimpleEsp8266.h>//Calls Blynk ESP8266 Library
 
 int sensorPin = A0;    // select the input pin for the potentiometer
 int ledPin = D7;      // select the pin for the LED
@@ -39,35 +40,81 @@ int Y = 0;
 void setup() {
   // declare the ledPin as an OUTPUT:
   pinMode(ledPin, OUTPUT);
-  // put your setup code here, to run once:
-  pinMode(X_CTRL, OUTPUT);
-  pinMode(Y_CTRL, OUTPUT);
-  digitalWrite(X_CTRL, LOW);
-  digitalWrite(Y_CTRL, LOW);
+//  // put your setup code here, to run once:
+//  pinMode(X_CTRL, OUTPUT);
+//  pinMode(Y_CTRL, OUTPUT);
+//  digitalWrite(X_CTRL, LOW);
+//  digitalWrite(Y_CTRL, LOW);
   Serial.begin(115200);
   Serial.println("");
   Serial.println("PASS: Serial communication started.");
+  calibrate();
 }
-void read_adc(){
+//void read_adc(){
+//
+//  // put your main code here, to run repeatedly:
+//  digitalWrite(X_CTRL, HIGH);
+//  delay(10);
+//  //digitalWrite(Y_CTRL, LOW);
+//  X = analogRead(ADC);
+//  digitalWrite(X_CTRL, LOW);
+//  delay(10);
+//  digitalWrite(Y_CTRL, HIGH);
+//  delay(10);
+//  Y = analogRead(ADC);
+//  digitalWrite(Y_CTRL, LOW);
+//  delay(10);
+//}
 
-  // put your main code here, to run repeatedly:
-  digitalWrite(X_CTRL, HIGH);
-  delay(10);
-  //digitalWrite(Y_CTRL, LOW);
-  X = analogRead(ADC);
-  digitalWrite(X_CTRL, LOW);
-  delay(10);
-  digitalWrite(Y_CTRL, HIGH);
-  delay(10);
-  Y = analogRead(ADC);
-  digitalWrite(Y_CTRL, LOW);
-  delay(10);
+int    joystick_xmax = 1023;
+float  joystick_xmult = 1;
+int    joystick_ymax = 1023;
+float  joystick_ymult = 1;
+
+static void calibrate()
+{
+  int cnt = 0;
+  int xmax = 0;
+  int xmin = 1023;
+  int ymax = 0;
+  int ymin = 1023;
+  while(cnt < 4)
+  {
+     int jx = joystick_xmax-get_joystick_x();
+     int jy = joystick_ymax-get_joystick_y();
+     if((cnt % 2 == 0) && jx < 450) 
+       cnt++;
+     if((cnt % 2 == 1) && jx > 800) 
+       cnt++;
+     xmax = MAX(xmax, jx);
+     xmin = MIN(xmin, jx);
+     ymax = MAX(ymax, jy);
+     ymin = MIN(ymin, jy);
+  }
+  joystick_xmax = xmax;
+  joystick_xmult = 1023.0/(xmax - xmin);
+  joystick_ymax = ymax;
+  joystick_ymult = 1023.0/(ymax - ymin);
+}
+static unsigned int get_joystick_x()
+{
+  pinMode(X_CTRL,INPUT);
+  pinMode(Y_CTRL,OUTPUT);
+  digitalWrite(Y_CTRL,LOW);
+  return (unsigned int)MAX(0, MIN(1023,((joystick_xmax-analogRead(ADC))*joystick_xmult)));
+}
+static unsigned int get_joystick_y()
+{
+  pinMode(Y_CTRL,INPUT);
+  pinMode(X_CTRL,OUTPUT);
+  digitalWrite(X_CTRL,LOW);
+  return (unsigned int)MAX(0, MIN(1023,((joystick_ymax-analogRead(ADC))*joystick_ymult)));
 }
 void loop() {
   digitalWrite(ledPin, HIGH);
-  read_adc();
-  Serial.println("INFO: sensor value X:" + String(X) + " Y:" + String(Y));
-  delay(50);
+  //read_adc();
+  Serial.println("INFO: sensor value X:" + String(get_joystick_x()) + " Y:" + String(get_joystick_y()));
+  //delay(50);
   digitalWrite(ledPin, LOW);
-  delay(50);
+  delay(150);
 }
