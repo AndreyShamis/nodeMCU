7e6f752126d61709e53ae7050a86a5b2e1175514
Andrey Shamis <lolnik@gmail.com>
2017-05-15 10:28:37 +0300

    Add MAX6675 library
    Arduino library for interfacing with MAX6675 thermocouple amplifier
    https://github.com/adafruit/MAX6675-library
    Added another library from https://github.com/SirUli/MAX6675
    which works
    
    Signed-off-by: Andrey Shamis <lolnik@gmail.com>

---
+++ Oven/Oven.ino
@@ -0,0 +1,39 @@
+
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266mDNS.h>
+// #define _delay_ms(ms) delayMicroseconds((ms) * 1000)
+#define ESP8266
+#include <max6675.h>
+
+int thermoDO    = 4;
+int thermoCS    = 5;
+int thermoCLK   = 6;
+
+MAX6675 thermocouple(thermoCLK, thermoCS, thermoDO);
+int vccPin = 3;
+int gndPin = 2;
+
+void setup() {
+  Serial.begin(9600);
+  // use Arduino pins
+  pinMode(vccPin, OUTPUT); digitalWrite(vccPin, HIGH);
+  pinMode(gndPin, OUTPUT); digitalWrite(gndPin, LOW);
+
+  Serial.println("MAX6675 test");
+  // wait for MAX chip to stabilize
+  delay(500);
+}
+
+void loop() {
+  // basic readout test, just print the current temp
+
+  Serial.print("C = ");
+  Serial.println(thermocouple.readCelsius());
+  Serial.print("F = ");
+  Serial.println(thermocouple.readFahrenheit());
+
+  delay(1000);
+}
+
