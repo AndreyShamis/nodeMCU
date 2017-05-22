3332018f0e4d19f5fe7eff178385bfab20a91584
Andrey Shamis <lolnik@gmail.com>
2017-05-22 16:21:11 +0300

    Change in socket_test
    Signed-off-by: Andrey Shamis <lolnik@gmail.com>

--- micropython/socket_test.py
+++ micropython/socket_test.py
@@ -1,6 +1,9 @@
 import socket
 import os
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(["192.168.1.1", 80])
+try:
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect(["192.168.1.1", 80])
+except Exception as ex:
+    print("Issue found :" + str(ex))
 s.close()
