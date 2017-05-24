6c5b4618d3b6ea0a45a0fa3d64bacedcf29e5451
Andrey Shamis <lolnik@gmail.com>
2017-05-24 12:28:40 +0300

    Test ADS1015
    Signed-off-by: Andrey Shamis <lolnik@gmail.com>

--- Joystick/Joystick/Joystick.ino
+++ Joystick/Joystick/Joystick.ino
@@ -26,6 +26,10 @@
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
 #include <BlynkSimpleEsp8266.h>//Calls Blynk ESP8266 Library
+#include <Wire.h>
+#include <Adafruit_ADS1015.h>
+
+Adafruit_ADS1015 ads;     /* Use thi for the 12-bit version */
 
 int sensorPin = A0;    // select the input pin for the potentiometer
 int ledPin = D7;      // select the pin for the LED
@@ -47,27 +51,47 @@ void setup() {
   Serial.begin(115200);
   Serial.println("");
   Serial.println("PASS: Serial communication started.");
+    ads.begin();
 }
-void read_adc() {
 
-  // put your main code here, to run repeatedly:
-  digitalWrite(X_CTRL, HIGH);
-  delay(1);
-  //digitalWrite(Y_CTRL, LOW);
-  X = analogRead(ADC);
-  digitalWrite(X_CTRL, LOW);
-  delay(2);
-  digitalWrite(Y_CTRL, HIGH);
-  delay(1);
-  Y = analogRead(ADC);
-  digitalWrite(Y_CTRL, LOW);
-  //delay(1);
-}
+//void read_adc() {
+//
+//  // put your main code here, to run repeatedly:
+//  digitalWrite(X_CTRL, HIGH);
+//  delay(1);
+//  //digitalWrite(Y_CTRL, LOW);
+//  X = analogRead(ADC);
+//  digitalWrite(X_CTRL, LOW);
+//  delay(2);
+//  digitalWrite(Y_CTRL, HIGH);
+//  delay(1);
+//  Y = analogRead(ADC);
+//  digitalWrite(Y_CTRL, LOW);
+//  //delay(1);
+//
+//  
+//}
+
 void loop() {
-  //digitalWrite(ledPin, HIGH);
-  read_adc();
-  Serial.println("INFO: sensor value X:" + String(X) + " Y:" + String(Y));
-  delay(50);
-  //digitalWrite(ledPin, LOW);
-  delay(50);
+//  //digitalWrite(ledPin, HIGH);
+//  read_adc();
+//  Serial.println("INFO: sensor value X:" + String(X) + " Y:" + String(Y));
+//  delay(50);
+//  //digitalWrite(ledPin, LOW);
+//  delay(50);
+
+    int16_t adc0, adc1, adc2, adc3;
+
+  adc0 = ads.readADC_SingleEnded(0);
+  adc1 = ads.readADC_SingleEnded(1);
+  adc2 = ads.readADC_SingleEnded(2);
+  adc3 = ads.readADC_SingleEnded(3);
+  Serial.print("AIN0: "); Serial.println(adc0);
+  Serial.print("AIN1: "); Serial.println(adc1);
+  Serial.print("AIN2: "); Serial.println(adc2);
+  Serial.print("AIN3: "); Serial.println(adc3);
+  Serial.println(" ");
+  
+  delay(1000);
 }
+
